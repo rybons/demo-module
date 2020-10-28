ebbef082858fb5ec5a9cee52a8ce277a110edac5
rybons <bonnerrk@gmail.com>
2020-10-28 15:15:45 -0400

    add output for subnet id

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "vnet_id" {
     value = azurerm_virtual_network.example.id
     description = "The ID of the VNET"
 }
+
+output "vnet_id" {
+    value = azurerm_subnet.example.id
+    description = "The ID of the SUBNET"
+}
